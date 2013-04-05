c8133201b85f6d515409267889396b3ff51a1f8f
Alan Witkowski <alan.witkowski@gmail.com>
2013-04-04 21:40:28 -0600

    some fixes

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,10 @@
 #include <BulletCollision/CollisionShapes/btTriangleMeshShape.h>
 #include <cstdio>
 
+const bool USE_INTERNAL_EDGE_UTILITY = true;
+const bool USE_STATIC_BOX = false;
+const btScalar TIMESTEP = 1.0 / 100.0;
+
 static btDefaultCollisionConfiguration *CollisionConfiguration;
 static btCollisionDispatcher *Dispatcher;
 static btBroadphaseInterface *BroadPhase;
@@ -15,8 +19,6 @@ static btRigidBody *BoxBody = NULL;
 static btRigidBody *MeshBody = NULL;
 static btTriangleIndexVertexArray *TriangleIndexVertexArray = NULL;
 static btTriangleInfoMap *TriangleInfoMap = NULL;
-
-static btScalar TimeStep = 1.0f / 100.0f;
 
 static btScalar Vertices[] = {
 	2.000000, 0.000000, 2.000000,
@@ -36,7 +38,7 @@ static bool CustomMaterialCallback(btManifoldPoint &ManifoldPoint, const btColli
 		return false;
 		
 	btScalar Before = ManifoldPoint.m_normalWorldOnB.getY();
-	if(1) {
+	if(USE_INTERNAL_EDGE_UTILITY) {
 		btAdjustInternalEdgeContacts(ManifoldPoint, Object1, Object0, PartID1, Index1);
 	}
 	btScalar After = ManifoldPoint.m_normalWorldOnB.getY();
@@ -54,43 +56,43 @@ int main(int ArgumentCount, char **Arguments) {
 	Dispatcher = new btCollisionDispatcher(CollisionConfiguration);
 	Solver = new btSequentialImpulseConstraintSolver();
 	World = new btDiscreteDynamicsWorld(Dispatcher, BroadPhase, Solver, CollisionConfiguration);
-	World->setGravity(btVector3(0.0f, -9.81f, 0.0f));
+	World->setGravity(btVector3(0.0, -9.81, 0.0));
 	
 	gContactAddedCallback = CustomMaterialCallback;
 	
 	{
-		btScalar Mass = 1.0f;
-		btCollisionShape *Shape = new btSphereShape(0.5f);
-		btVector3 LocalInertia(0.0f, 0.0f, 0.0f);
+		btScalar Mass = 1.0;
+		btCollisionShape *Shape = new btSphereShape(0.5);
+		btVector3 LocalInertia(0.0, 0.0, 0.0);
 		Shape->calculateLocalInertia(Mass, LocalInertia);
 		
 		SphereBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
-		SphereBody->setFriction(1.0f);
-		SphereBody->setDamping(0.1f, 0.3f);
+		SphereBody->setFriction(1.0);
+		SphereBody->setDamping(0.1, 0.3);
 		SphereBody->setActivationState(DISABLE_DEACTIVATION);
-		SphereBody->translate(btVector3(0.71f, 2.5, -0.8f));
+		SphereBody->translate(btVector3(0.71, 2.5, -0.8));
 		
 		World->addRigidBody(SphereBody);
 	}
 	
 	// Set to 1 to test static box shape
-	if(0) {
+	if(USE_STATIC_BOX) {
 		
-		btScalar Mass = 0.0f;
+		btScalar Mass = 0.0;
 		btCollisionShape *Shape = new btBoxShape(btVector3(1, 1, 1));
-		btVector3 LocalInertia(0.0f, 0.0f, 0.0f);
+		btVector3 LocalInertia(0.0, 0.0, 0.0);
 		Shape->calculateLocalInertia(Mass, LocalInertia);
 		
 		BoxBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
-		BoxBody->setFriction(1.0f);
-		BoxBody->setDamping(0.1f, 0.3f);
+		BoxBody->setFriction(1.0);
+		BoxBody->setDamping(0.1, 0.3);
 		BoxBody->setActivationState(DISABLE_DEACTIVATION);
 		BoxBody->translate(btVector3(0, -2, 0));
 		
 		World->addRigidBody(BoxBody);
 	}
 	else {
-		btScalar Mass = 0.0f;
+		btScalar Mass = 0.0;
 		int VertexCount = sizeof(Vertices) / sizeof(btScalar);
 		int FaceCount = sizeof(Indices) / sizeof(int) / 3;
 		TriangleIndexVertexArray = new btTriangleIndexVertexArray(FaceCount, Indices, 3 * sizeof(int), VertexCount, Vertices, 3 * sizeof(btScalar));
@@ -99,12 +101,9 @@ int main(int ArgumentCount, char **Arguments) {
 		btTriangleInfoMap *TriangleInfoMap = new btTriangleInfoMap();
 		btGenerateInternalEdgeInfo(Shape, TriangleInfoMap);
 	
-		btVector3 LocalInertia(0.0f, 0.0f, 0.0f);
-		Shape->calculateLocalInertia(Mass, LocalInertia);
-		
-		MeshBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
-		MeshBody->setFriction(1.0f);
-		MeshBody->setDamping(0.1f, 0.3f);
+		MeshBody = new btRigidBody(Mass, NULL, Shape);
+		MeshBody->setFriction(1.0);
+		MeshBody->setDamping(0.1, 0.3);
 		MeshBody->setCollisionFlags(MeshBody->getCollisionFlags() | btCollisionObject::CF_CUSTOM_MATERIAL_CALLBACK);
 
 		// Add body
@@ -112,7 +111,7 @@ int main(int ArgumentCount, char **Arguments) {
 	}
 	
 	for(int i = 0; i < 100; i++) {
-		World->stepSimulation(TimeStep, 0, 0);
+		World->stepSimulation(TIMESTEP, 0, 0);
 	
 		const btTransform &Transform = SphereBody->getCenterOfMassTransform();
 		const btVector3 &Position = Transform.getOrigin();
