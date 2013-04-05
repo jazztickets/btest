26134433b9b0476d0c110514b59a4902470152e9
Alan Witkowski <alan.witkowski@gmail.com>
2013-04-04 21:22:05 -0600

    added static box test

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,12 @@ static btSequentialImpulseConstraintSolver *Solver;
 static btDiscreteDynamicsWorld *World;
 
 static btRigidBody *SphereBody = NULL;
+static btRigidBody *BoxBody = NULL;
 static btRigidBody *MeshBody = NULL;
-static btTriangleIndexVertexArray *TriangleIndexVertexArray;
-static btTriangleInfoMap *TriangleInfoMap;
+static btTriangleIndexVertexArray *TriangleIndexVertexArray = NULL;
+static btTriangleInfoMap *TriangleInfoMap = NULL;
 
 static btScalar TimeStep = 1.0f / 100.0f;
-static btScalar TimeStepAccumulator = 0.0f;
 
 static btScalar Vertices[] = {
 	2.000000, 0.000000, 2.000000,
@@ -26,8 +26,8 @@ static btScalar Vertices[] = {
 };
 
 static int Indices[] = {
-	3, 1, 2,
 	1, 0, 2,
+	3, 1, 2,
 };
 
 static bool CustomMaterialCallback(btManifoldPoint &ManifoldPoint, const btCollisionObjectWrapper *Object0, int PartID0, int Index0, const btCollisionObjectWrapper *Object1, int PartID1, int Index1) {
@@ -40,7 +40,7 @@ static bool CustomMaterialCallback(btManifoldPoint &ManifoldPoint, const btColli
 		btAdjustInternalEdgeContacts(ManifoldPoint, Object1, Object0, PartID1, Index1);
 	}
 	btScalar After = ManifoldPoint.m_normalWorldOnB.getY();
-	if(Before != After) printf("before y=%f after y=%f\n", Before, After);
+	if(Before != After) printf("contact callback: before y=%f after y=%f\n", Before, After);
 
 	return false;
 }
@@ -67,14 +67,29 @@ int main(int ArgumentCount, char **Arguments) {
 		SphereBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
 		SphereBody->setFriction(1.0f);
 		SphereBody->setDamping(0.1f, 0.3f);
-		SphereBody->setSleepingThresholds(0.2f, 0.2f);
 		SphereBody->setActivationState(DISABLE_DEACTIVATION);
-		SphereBody->translate(btVector3(0, 2.5, 0));
+		SphereBody->translate(btVector3(0.71f, 2.5, -0.8f));
 		
 		World->addRigidBody(SphereBody);
 	}
 	
-	{
+	// Set to 1 to test static box shape
+	if(0) {
+		
+		btScalar Mass = 0.0f;
+		btCollisionShape *Shape = new btBoxShape(btVector3(1, 1, 1));
+		btVector3 LocalInertia(0.0f, 0.0f, 0.0f);
+		Shape->calculateLocalInertia(Mass, LocalInertia);
+		
+		BoxBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
+		BoxBody->setFriction(1.0f);
+		BoxBody->setDamping(0.1f, 0.3f);
+		BoxBody->setActivationState(DISABLE_DEACTIVATION);
+		BoxBody->translate(btVector3(0, -2, 0));
+		
+		World->addRigidBody(BoxBody);
+	}
+	else {
 		btScalar Mass = 0.0f;
 		int VertexCount = sizeof(Vertices) / sizeof(btScalar);
 		int FaceCount = sizeof(Indices) / sizeof(int) / 3;
@@ -94,7 +109,6 @@ int main(int ArgumentCount, char **Arguments) {
 
 		// Add body
 		World->addRigidBody(MeshBody);
-	
 	}
 	
 	for(int i = 0; i < 100; i++) {
@@ -105,16 +119,26 @@ int main(int ArgumentCount, char **Arguments) {
 		printf("%f %f %f\n", Position.getX(), Position.getY(), Position.getZ());
 	}
 	
-	
 	World->removeRigidBody(SphereBody);
-	World->removeRigidBody(MeshBody);
+	if(MeshBody)
+		World->removeRigidBody(MeshBody);
+	if(BoxBody)
+		World->removeRigidBody(BoxBody);
+		
 	delete SphereBody->getCollisionShape();	
 	delete SphereBody;
 
-	delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getTriangleInfoMap();	
-	delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getMeshInterface();	
-	delete MeshBody->getCollisionShape();	
-	delete MeshBody;
+	if(MeshBody) {
+		delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getTriangleInfoMap();	
+		delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getMeshInterface();	
+		delete MeshBody->getCollisionShape();	
+		delete MeshBody;
+	}
+	
+	if(BoxBody) {
+		delete BoxBody->getCollisionShape();	
+		delete BoxBody;
+	}
 	
 	delete World;
 	delete Solver;
