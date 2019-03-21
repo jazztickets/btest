a0c5c7d904aeed73f936b4630abf02169dc51d2c
Alan Witkowski <alan.witkowski@gmail.com>
2019-03-21 09:17:17 -0600

    clean up

--- rename.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-grep btest -Ril . --exclude-dir=.git | xargs sed -i "s/btest/$1/g"
-mv codelite/btest.project codelite/$1.project
-mv codelite/btest.workspace codelite/$1.workspace
--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <btBulletDynamicsCommon.h>
 #include <BulletCollision/CollisionDispatch/btInternalEdgeUtility.h>
 #include <BulletCollision/CollisionShapes/btTriangleMeshShape.h>
-#include <cstdio>
+#include <iostream>
 
 // culprit: Solver->m_tmpSolverBodyPool.m_data[1].m_deltaLinearVelocity.m_floats[0]
 // set at btSequentialImpulseConstraintSolver.cpp:1374
@@ -44,14 +44,16 @@ static bool CustomMaterialCallback(btManifoldPoint &ManifoldPoint, const btColli
 
 	if(Object1->getCollisionShape()->getShapeType() != TRIANGLE_SHAPE_PROXYTYPE)
 		return false;
-		
+
 	btScalar Before = ManifoldPoint.m_normalWorldOnB.getY();
 	if(USE_INTERNAL_EDGE_UTILITY) {
 		HitOnce = true;
 		btAdjustInternalEdgeContacts(ManifoldPoint, Object1, Object0, PartID1, Index1);
 	}
+
 	btScalar After = ManifoldPoint.m_normalWorldOnB.getY();
-	if(Before != After) printf("contact callback: before y=%f after y=%f\n", Before, After);
+	if(Before != After)
+		std::cout << "contact callback: before y=" << Before << " after y=" << After << std::endl;
 
 	return false;
 }
@@ -70,38 +72,38 @@ int main(int ArgumentCount, char **Arguments) {
 	//SolverInfo.m_splitImpulseTurnErp = 0.0f;
 	//SolverInfo.m_splitImpulse = 1;
 	//SolverInfo.m_splitImpulsePenetrationThreshold = -0.02f;
-	
+
 	gContactAddedCallback = CustomMaterialCallback;
-	
+
 	{
 		btScalar Mass = 1.0;
 		btCollisionShape *Shape = new btSphereShape(0.5);
 		btVector3 LocalInertia(0.0, 0.0, 0.0);
 		Shape->calculateLocalInertia(Mass, LocalInertia);
-		
+
 		SphereBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
 		SphereBody->setFriction(1.0);
 		SphereBody->setDamping(0.1, 0.3);
 		SphereBody->setActivationState(DISABLE_DEACTIVATION);
 		SphereBody->translate(btVector3(0.71, 2.5, -0.8));
-		
+
 		World->addRigidBody(SphereBody);
 	}
-	
+
 	// Set to 1 to test static box shape
 	if(USE_STATIC_BOX) {
-		
+
 		btScalar Mass = 0.0;
 		btCollisionShape *Shape = new btBoxShape(btVector3(1, 1, 1));
 		btVector3 LocalInertia(0.0, 0.0, 0.0);
 		Shape->calculateLocalInertia(Mass, LocalInertia);
-		
+
 		BoxBody = new btRigidBody(Mass, NULL, Shape, LocalInertia);
 		BoxBody->setFriction(1.0);
 		BoxBody->setDamping(0.1, 0.3);
 		BoxBody->setActivationState(DISABLE_DEACTIVATION);
 		BoxBody->translate(btVector3(0, -2, 0));
-		
+
 		World->addRigidBody(BoxBody);
 	}
 	else {
@@ -113,7 +115,7 @@ int main(int ArgumentCount, char **Arguments) {
 		btBvhTriangleMeshShape *Shape = new btBvhTriangleMeshShape(TriangleIndexVertexArray, true);
 		btTriangleInfoMap *TriangleInfoMap = new btTriangleInfoMap();
 		btGenerateInternalEdgeInfo(Shape, TriangleInfoMap);
-	
+
 		MeshBody = new btRigidBody(Mass, NULL, Shape);
 		MeshBody->setFriction(1.0);
 		MeshBody->setDamping(0.1, 0.3);
@@ -122,36 +124,36 @@ int main(int ArgumentCount, char **Arguments) {
 		// Add body
 		World->addRigidBody(MeshBody);
 	}
-	
+
 	for(int i = 0; i < 100; i++) {
 		World->stepSimulation(TIMESTEP, 0, 0);
-	
+
 		const btTransform &Transform = SphereBody->getCenterOfMassTransform();
 		const btVector3 &Position = Transform.getOrigin();
-		printf("%f %f %f\n", Position.getX(), Position.getY(), Position.getZ());
+		std::cout << Position.getX() << " " << Position.getY() << " " << Position.getZ() << std::endl;
 	}
-	
+
 	World->removeRigidBody(SphereBody);
 	if(MeshBody)
 		World->removeRigidBody(MeshBody);
 	if(BoxBody)
 		World->removeRigidBody(BoxBody);
-		
-	delete SphereBody->getCollisionShape();	
+
+	delete SphereBody->getCollisionShape();
 	delete SphereBody;
 
 	if(MeshBody) {
-		delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getTriangleInfoMap();	
-		delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getMeshInterface();	
-		delete MeshBody->getCollisionShape();	
+		delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getTriangleInfoMap();
+		delete ((btBvhTriangleMeshShape *)MeshBody->getCollisionShape())->getMeshInterface();
+		delete MeshBody->getCollisionShape();
 		delete MeshBody;
 	}
-	
+
 	if(BoxBody) {
-		delete BoxBody->getCollisionShape();	
+		delete BoxBody->getCollisionShape();
 		delete BoxBody;
 	}
-	
+
 	delete World;
 	delete Solver;
 	delete Dispatcher;
